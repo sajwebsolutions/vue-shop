a1e2b64fb47167abd83d777f97c198377307977b
Saj <sajidwebservices@gmail.com>
2022-11-14 14:23:54 +0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+    import { createApp } from 'vue';
+    import store from "@/store/index";
+    import router from './router.js';
+    import App from './App.vue';
+    import BaseBadge from './components/ui/BaseBadge.vue';
+
+    const app = createApp(App);
+
+    app.use(router);
+    app.use(store);
+
+    app.component('base-badge', BaseBadge);
+
+    app.mount('#app');
